e387eccfe65b6faa1a2872033acdc07c99ac59a5
ArseniyCool <laptevarseniy59@mail.ru>
2026-03-06 14:08:48 +0300

    Commit

--- src/main/kotlin/blacksun/services/SurveyService.kt
+++ src/main/kotlin/blacksun/services/SurveyService.kt
@@ -5,6 +5,19 @@ import jakarta.inject.Inject
 import org.telegram.telegrambots.meta.api.objects.Update //основной контейнер события
 import org.telegram.telegrambots.meta.api.objects.Message //формирование ответа
 
+enum class Answers(val text: String) {
+    WELCOME("Добро пожаловать!\n\nПожалуйста, отправьте номер телефона."),
+    PONG("Понг!"),
+    DONTUNDERSTAND("Я не понимаю эту команду"),
+    NUMBERSAVED("Телефон сохранен! Введите название команды!"),
+    INCORRECTNUMBER("Введен неправильный номер. Повторите попытку."),
+    PROJECTSAVED("Название проекта сохранео. Спасибо!")
+}
+
+enum class UserStates {
+    WAITING_FOR_PHONE,
+    WAITING_FOR_PROJECT_NAME
+}
 
 @Singleton //одиночный класс, класс-сервис, глобальный сервис
 
@@ -15,7 +28,14 @@ import org.telegram.telegrambots.meta.api.objects.Message //формирован
 Произошло любое действие с ботом
 ➡ Telegram формирует объект Update и отправляет его твоему серверу.
 */
+
 class SurveyService {
+    val userStates = mutableMapOf<Long, UserStates>()
+
+    private fun isValidPhoneNumber(phoneNumber: String): Boolean {
+//    if (phoneNumber.isEmpty()) return false
+        return true //TODO("Provide the return value")
+    }
     fun handle(update: Update): Message {
 
         val chatId = update.message.chatId //id пользователя
@@ -23,14 +43,47 @@ class SurveyService {
 
         val response = Message() //объект ответа
 
+        // ===== 1. Глобальные команды (высший приоритет)
         when (text) {
-            "/start" -> response.text = "Добро пожаловать!"
 
-            "/ping" -> response.text = "Pong!"
+            "/start" -> {
+                userStates[chatId] = UserStates.WAITING_FOR_PHONE
+                response.text = Answers.WELCOME.text
+                return response
+            }
 
-            else -> response.text = "Я не понимаю эту команду"
+            "/ping" -> {
+                response.text = "Понг!"
+                return response
+            }
         }
+        // ===== 2. Если пользователь в состоянии
+        val state = userStates[chatId]
 
+        if (state != null) {
+            when (state) {
+
+                UserStates.WAITING_FOR_PHONE -> {
+
+                    if (!isValidPhoneNumber(text)) {
+                        response.text = Answers.INCORRECTNUMBER.text
+                        return response
+                    }
+
+                    response.text = Answers.NUMBERSAVED.text
+                    userStates[chatId] = UserStates.WAITING_FOR_PROJECT_NAME
+                }
+
+                UserStates.WAITING_FOR_PROJECT_NAME -> {
+                    response.text = Answers.PROJECTSAVED.text
+                    userStates.remove(chatId)
+                }
+            }
+
+            return response
+        }
+        // ===== 3. Fallback
+        response.text = Answers.DONTUNDERSTAND.text
         return response
     }
 }
--- src/test/kotlin/blacksun/services/SurveyServiceTest.kt
+++ src/test/kotlin/blacksun/services/SurveyServiceTest.kt
@@ -17,29 +17,58 @@ class SurveyServiceTest {
 
     @field:Inject
     lateinit var service: SurveyService
+    //lateinit - инициализируется позже
+    //Kotlin имеет: property field getter setter
+    //Micronaut работает именно с field.
+    //Inject ("вставить") автоматически подставить объект.
+
+    private fun mockUpdate(text: String): Update {
+        val update = mockk<Update>()
+        val message = mockk<Message>()
+
+        every { message.text } returns text
+        every { message.chatId } returns 1L
+        every { update.message } returns message
+
+        return update
+    }
 
     @Test
     fun `start command should return hello`() {
-        val update = mockk<Update>() //фейк апдейт
-        val message = mockk<Message>() //фейк сообщение
-        every { update.message} returns message
-        every { message.text } returns "/start"
-        every { message.chatId } returns 1L //число Long
-
-        val response = service.handle(update)
-
-        assertEquals("Добро пожаловать!", response.text)
+        val startResponse = service.handle(mockUpdate("/start"))
+        assertEquals(Answers.WELCOME.text, startResponse.text)
     }
     @Test
+    fun `phone command should return number saved`() {
+        service.handle(mockUpdate("/start"))
+
+        val phoneResponse = service.handle(mockUpdate("82143965701"))
+        assertEquals(Answers.NUMBERSAVED.text, phoneResponse.text)
+    }
+    @Test
+    fun `project command should return project saved`() {
+        service.handle(mockUpdate("/start"))
+        service.handle(mockUpdate("82143965701"))
+
+        val projectResponse = service.handle(mockUpdate("My Project"))
+        assertEquals(Answers.PROJECTSAVED.text, projectResponse.text)
+    }
+
+    @Test
+    fun `caps command should go through too`() {
+        val response = service.handle(mockUpdate("/StArT"))
+        assertEquals(Answers.WELCOME.text, response.text)
+    }
+
+    @Test
     fun `ping command should return pong`() {
-        val update = mockk<Update>()
-        val message = mockk<Message>()
-        every { update.message} returns message
-        every { message.text } returns "/ping"
-        every { message.chatId } returns 1L
+        val response = service.handle(mockUpdate("/ping"))
+        assertEquals(Answers.PONG.text, response.text)
+    }
 
-        val response = service.handle(update)
-
-        assertEquals("Pong!", response.text)
+    @Test
+    fun `nofind command should return not found`() {
+        val response = service.handle(mockUpdate("something"))
+        assertEquals(Answers.DONTUNDERSTAND.text, response.text)
     }
 }
